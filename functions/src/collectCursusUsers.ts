ad46989ee90073493363f03565d262c980c48957
nafuka <42476527+nafuka11@users.noreply.github.com>
2022-05-29 01:06:38 +0900

    feat: sanitize CursusUser

--- functions/src/collectCursusUsers.ts
+++ functions/src/collectCursusUsers.ts
@@ -1,11 +1,14 @@
 import { fetchAccessToken, getCursusUsers, initClient } from "./ftApi";
 import { Bucket } from "@google-cloud/storage";
 import { format } from "date-fns";
+import { sanitizeCursusUsers } from "./sanitize";
 
 export const saveCursusUsersToStorage = async (bucket: Bucket) => {
   const client = initClient();
   const accessToken = await fetchAccessToken(client);
   const cursusUsers = await getCursusUsers(accessToken);
+  sanitizeCursusUsers(cursusUsers);
+
   const dateStr = format(new Date(), "yyyy-MM-dd");
   await bucket
     .file(`cursus_users/${dateStr}.json`)
