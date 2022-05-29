a2b930c0328feadc0ab8ecb7d76c0170e1586603
nafuka <42476527+nafuka11@users.noreply.github.com>
2022-05-29 19:58:41 +0900

    feat: get bucket name from environment variable

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,9 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import { getStorage } from "firebase-admin/storage";
+import { saveCursusUsers } from "./collectCursusUsers";
 import { SCHEDULE, TIMEOUT_SECONDS, TIMEZONE } from "./constants";
+import { getEnv } from "./utils/getEnv";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -13,7 +15,7 @@ export const collectCursusUsers = functions
   .pubsub.schedule(SCHEDULE)
   .timeZone(TIMEZONE)
   .onRun(async () => {
-    const bucket = getStorage().bucket();
-    await saveCursusUsersToStorage(bucket);
+    const bucket = getStorage().bucket(getEnv("BUCKET_NAME"));
+    await saveCursusUsers(bucket);
     return null;
   });
